1e1dd78f3bcbfc5f1a206a740373f689dbe22169
nazat47 <nazat.mf@gmail.com>
2024-02-27 10:15:27 +0600

    updated ui

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -10,7 +10,7 @@ const ProfilePage = () => {
     <div>
       <Header />
       <div className={`${styles.section} flex bg-[#f2f5fa] py-10`}>
-        <div className="w-[50px] 800px:w-[335px] sticky 800px:mt-[0] mt-[18%]">
+        <div className="w-[50px] 800px:w-[335px] 800px:mt-[0] mt-[18%]">
           <ProfileSidebar active={active} setActive={setActive} />
         </div>
         <ProfileContent active={active}/>
